496a99ed78ab286f9da7bdf049c8c50fb50fb9a7
mahlad <mahlad365@gmail.com>
2012-08-29 19:24:31 +0430

    corect the addresses

--- mahlad-PartofImg-Slideshow-02/index.php
+++ mahlad-PartofImg-Slideshow-02/index.php
@@ -4,16 +4,16 @@
 	<meta charset="UTF-8">
 	<title>Part of Image Slideshow</title>
 	<link rel="stylesheet" type="text/css" href="../reset.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="../zepto.js"></script>
-	<script type="text/javascript" src="script/partofImg.js"></script>
+	<script type="text/javascript" src="partofImg.js"></script>
 </head>
 <body>
 	<div class="container">
 		<div class="partSl">
 			<div class="imgSl">
 				<?php 
-					$path='./images';
+					$path='../images/mahlad-PartofImg-Slideshow';
 					$img_arr=scandir($path);
 					
 					foreach ($img_arr as $each_img) {
